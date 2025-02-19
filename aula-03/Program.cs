ea1de4b43a7cd54ecba08f3a4ae00a6d50027282
henrique0120 <140437860+henrique0120@users.noreply.github.com>
2025-02-19 14:28:57 +0000

    feat: adicionar métodos para gestão de volume

---
+++ aula-03/Program.cs
@@ -0,0 +1,19 @@
+﻿﻿﻿using aula_03;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int idade = 32;
+
+Televisao tvSala = new Televisao(22f);
+Console.WriteLine($"O tamanho da tv é: {tvSala.Tamanho}");
+
+Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
+tvSala.AumentarVolume();
+
+Console.WriteLine($"O tamanho da tv é: {tvSala.Tamanho}");
+
+
+tvSala.AumentarVolume();
+Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
+tvSala.AumentarVolume();
